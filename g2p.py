0c87e7781374154ce2ab7e8f3d8863526af7c198
Liam Graham <liam.david.graham@gmail.com>
2020-09-19 20:41:51 +1000

    Implement input-to-output phoneme mapping

--- g2p.py
+++ g2p.py
@@ -5,7 +5,7 @@ Given a word list in a target language for which no specific model exists, find 
 import sh
 import sys
 import os
-from typing import Dict, Set, List
+from typing import Dict, Set, List, Tuple
 
 
 phon = sh.phonetisaurus_apply
@@ -18,9 +18,7 @@ LANG_PATH = "/home/liam/university/2020/engg4801/anylang/lang2lang/lang.dists"
 PHON_PATH = "/home/liam/university/2020/engg4801/anylang/phon2phon/ipa.bitdist.wfst"
 DATA_PATH = "/home/liam/university/2020/engg4801/g2p/data"
 OUTPUT_PATH = "/home/liam/university/2020/engg4801/g2p/output"
-TABLE_PATH = "/home/liam/university/2020/engg4801/anylang/phon2phon/ipa.bitdist.table"
 
-STATE_NAME = "S"
 # phonemic distance between a target language (given the phonemic inventory of that language) and a langauge in the model set
 
 
@@ -64,7 +62,7 @@ class Lang2Lang:
 
 class Transition:
     """
-    Single-state transition on 'in_lable' to 'out_label' with the given weight.
+    Single-state transition on 'in_label' to 'out_label' with the given weight.
     """
 
     def __init__(self, state, in_label, out_label, weight=1):
@@ -78,6 +76,56 @@ class Transition:
 
     def __str__(self):
         return self.__repr__()
+
+
+class Mapping:
+    """
+    Uses a single-state WFST to map phonemes from an input inventory to those in an output inventory.
+    """
+    TABLE_PATH = "/home/liam/university/2020/engg4801/anylang/phon2phon/ipa.bitdist.table"
+    STATE_NAME = "S"
+
+    def __init__(self, in_path: str, out_path: str, map_path: str):
+        self.input = get_inventory(in_path)
+        self.output = get_inventory(out_path)
+        self.map_path = map_path
+        self._save_mappings()
+
+    def get_closest(self, phoneme: str) -> Tuple[str, float]:
+        """
+        Returns the phoneme in the output inventory, as well as the corresponding distance, that is most similar to the given phoneme
+        in the input inventory.
+        """
+        if phoneme not in self.input:
+            raise ValueError(f"'{phoneme}' is not in the input inventory")
+        result = carmel(sh.echo(f'"{phoneme}"'), "-silkOQ", 1, self.map_path).split(" ")
+        closest = result[0]
+        distance = float(result[1])
+        return closest, distance        
+
+    def _save_mappings(self) -> None:
+        """
+        Saves mappings for input and output inventories to .wfst file
+        """    
+        mappings = self._get_mappings()
+        with open(self.map_path, "w") as f:
+            f.write(self.STATE_NAME + "\n")
+            for m in mappings:
+                f.write(str(m) + "\n")
+    
+    def _get_mappings(self) -> Transition:
+        """
+        Yields all mappings in the phon2phon table from phonemes in the input inventory to phonemes
+        in the output inventory, skipping all others.
+        """
+        with open(self.TABLE_PATH, "r") as table:
+            for row in table:
+                in_label, out_label, weight = row.strip().split("\t")
+                if in_label not in self.input:
+                    continue
+                if out_label not in self.output:
+                    continue
+                yield Transition(self.STATE_NAME, in_label, out_label, float(weight))
 
 
 def get_lexicon(word_list, model) -> str:
@@ -96,57 +144,12 @@ def get_inventory(path: str) -> Set[str]:
     return inventory
 
 
-def generate_mapping(in_inv: Set[str], out_inv: Set[str], map_path: str) -> None:
-    """
-    S
-    (S (S "I_1" "O_1" 1.0))
-    (S (S "I_1" "O_2" 1.0))
-    (S (S "I_1" "O_3" 1.0))
-    ...
-    """
-    with open(map_path, "w") as mapping:
-        f.write(state + "\n")
-        for phon1 in in_inv:
-            for phon2 in out_inv:
-                trans = Transition(STATE_NAME, phon1, phon2)
-                mapping.write(str(trans) + "\n")
-
-
-def get_mappings(in_inv: Set[str], out_inv: Set[str], table_path: str):
-    """
-    Yields all mappings in the given table from phonemes in the input inventory to phonemes
-    in the output inventory, skipping all others.
-    """
-    with open(table_path, "r") as table:
-        for row in table:
-            # Format: "i o 0.001\n"
-            in_label, out_label, weight = row.strip().split("\t")
-            if in_label not in in_inv:
-                continue
-            if out_label not in out_inv:
-                continue
-            yield Transition(STATE_NAME, in_label, out_label, float(weight))
-
-
-def save_mappings(in_path: str, out_path: str, map_path: str):
-    """
-    Saves mappings for input and output inventories at the given paths.
-    """    
-    in_inv = get_inventory(in_path)
-    out_inv = get_inventory(out_path)
-    mappings = get_mappings(in_inv, out_inv, TABLE_PATH)
-    with open(map_path, "w") as f:
-        f.write(STATE_NAME + "\n")
-        for m in mappings:
-            f.write(str(m) + "\n")
-
-
-def get_closest_phoneme(phoneme, map_path):
-    carmel()
-
-
 if __name__ == "__main__":
-    #lang = Lang2Lang(LANG_PATH)
-    #print(lang.get_distances("aal", "aap"))
-    phon = Phon2Phon(PHON_PATH) 
-    phon.get_distances("a")
+    #mapping = Mapping(DATA_PATH + "/eng_2176.phon", DATA_PATH + "/deu_2184.phon", OUTPUT_PATH + "/eng_deu.wfst")
+    #phonemes = ["θ", "ɪ", "ŋ", "kʰ"]
+    
+    mapping = Mapping(DATA_PATH + "/ben_2162.phon", DATA_PATH + "/eng_2176.phon", OUTPUT_PATH + "/eng_deu.wfst")
+    phonemes = ["d̪ʱ", "a", "k", "a"]
+    for phon in phonemes:
+        closest, _ = mapping.get_closest(phon)
+        print(closest)
